2f26ce380bebf8c062bd9807085f23222b6dc040
Tyler Reed <valor13111@gmail.com>
2017-12-15 17:06:47 -0500

    Initial commit

---
+++ simple-todos/imports/ui/Task.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Component } from 'react';
+
+// Task component - represents a single todo item
+export  default class Task extends Component {
+  render() {
+    return (
+      <li>{this.props.task.text}</li>
+    );
+  }
+}
