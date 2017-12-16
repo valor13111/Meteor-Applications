3c2a42588dc920829c92d15682a9f24c0ccf9c23
Tyler Reed <valor13111@gmail.com>
2017-12-16 18:35:00 -0500

    Cleaned up code.

--- simple-todos/imports/ui/App.js
+++ simple-todos/imports/ui/App.js
@@ -28,11 +28,7 @@ class App extends Component {
   handleSubmit = (event) => {
     event.preventDefault();
 
-    // Find the text field via the React ref
-    const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
-
     Tasks.insert({
-      //text,
       text: this.state.newTodoValue,
       createdAt: new Date(), // current time
     });
@@ -41,7 +37,6 @@ class App extends Component {
     this.setState({
       newTodoValue: '',
     });
-    //ReactDOM.findDOMNode(this.refs.textInput).value = '';
   }
 
   renderTasks() {
