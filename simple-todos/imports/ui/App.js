bcd6b824c64cf4bd9b3ff594924662236d0e4aaf
Tyler Reed <valor13111@gmail.com>
2017-12-19 22:42:47 -0500

    Can check only to see incomplete tasks.

--- simple-todos/imports/ui/App.js
+++ simple-todos/imports/ui/App.js
@@ -9,22 +9,24 @@ import Task from './Task.js';
 
 // App component - representing whole App
 // Uses a task component to render the list of items
+// initialize states here
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       newTodoValue: '',
+      hideCompleted: false,
     };
   }
 
-
+  // handles the change of input box, and updates state
   handleChange = (event) => {
     this.setState({
       newTodoValue: event.target.value
     });
   }
 
-
+  // handles the submission by inserting new task into mongodb with date
   handleSubmit = (event) => {
     event.preventDefault();
 
@@ -39,8 +41,21 @@ class App extends Component {
     });
   }
 
+  // toggle the state of checkbox to ONLY show incomplete tasks
+  toggleHideCompleted = () => {
+    this.setState({
+      hideCompleted: !this.state.hideCompleted,
+    });
+  }
+
+  // render the tasks by Using a Task Component
   renderTasks() {
-    return this.props.tasks.map((task) => (
+    let filteredTasks = this.props.tasks;
+
+    if (this.state.hideCompleted) {
+      filteredTasks = filteredTasks.filter(task => !task.checked);
+    }
+    return filteredTasks.map((task) => (
       <Task key={task._id} task={task} />
     ));
   }
@@ -49,7 +64,17 @@ class App extends Component {
     return(
       <div className="container">
         <header>
-          <h1>Todo List</h1>
+          <h1>Todo List ({this.props.incompleteCount})</h1>
+
+          <label className="hide-completed">
+            <input
+              type="checkbox"
+              readOnly
+              checked={ this.state.hideCompleted }
+              onClick={ this.toggleHideCompleted.bind(this) }
+            />
+            Hide Completed Tasks
+          </label>
 
           <form className="new-task" onSubmit={ this.handleSubmit.bind(this) } >
             <input
@@ -73,5 +98,6 @@ class App extends Component {
 export default withTracker(() => {
   return {
     tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
+    incompleteCount: Tasks.find({ checked: { $ne: true } }).count(),
   };
 })(App);
